bee8ea51d7b7e6bde89769ec6a5f72f672752623
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2022-03-27 00:41:00 -0400

    Génération aléatoire d’individus

--- Main.cpp
+++ Main.cpp
@@ -31,7 +31,7 @@ void test_toString() {
 	Arbre<ColorCell> arbre;
 	arbre.racine = new Noeud<ColorCell>(2, B);
 	arbre.racine->ancGauche = new Noeud<ColorCell>(1, A);
-	arbre.racine->ancDroit = new Noeud<ColorCell>(3, C);
+	arbre.racine->ancDroite = new Noeud<ColorCell>(3, C);
 
 	cout << arbre.toString() << endl;
 }
@@ -74,14 +74,14 @@ void test_recherche() {
 	auto ND = new Noeud<ColorCell>(4, D);
 	auto NE = new Noeud<ColorCell>(5, E);
 	NB->ancGauche = ND;
-	NB->ancDroit = NE;
+	NB->ancDroite = NE;
 	auto NC = new Noeud<ColorCell>(3, C);
 	auto NF = new Noeud<ColorCell>(6, F);
 	NC->ancGauche = ND;
-	NC->ancDroit = NF;
+	NC->ancDroite = NF;
 
 	arbre.racine->ancGauche = NB;
-	arbre.racine->ancDroit = NC;
+	arbre.racine->ancDroite = NC;
 	
 	cout << arbre.toString() << endl;
 	cout << "resultat recherche : " << arbre.recherche(4) << endl;
@@ -107,20 +107,20 @@ void test_ancetreCommun() {
 	auto NF = new Noeud<ColorCell>(6, F);
 
 	NA->ancGauche = NB;
-	NA->ancDroit = NC;
+	NA->ancDroite = NC;
 	NB->ancGauche = ND;
-	NB->ancDroit = NE;
+	NB->ancDroite = NE;
 	NC->ancGauche = ND;
-	NC->ancDroit = NF;
+	NC->ancDroite = NF;
 	
 
 	Population<ColorCell> pop;
-	pop.insererFin(Arbre<ColorCell>(NA));
-	pop.insererFin(Arbre<ColorCell>(NB));
-	pop.insererFin(Arbre<ColorCell>(NC));
-	pop.insererFin(Arbre<ColorCell>(ND));
-	pop.insererFin(Arbre<ColorCell>(NE));
-	pop.insererFin(Arbre<ColorCell>(NF));
+	pop.insererFin(NA);
+	pop.insererFin(NB);
+	pop.insererFin(NC);
+	pop.insererFin(ND);
+	pop.insererFin(NE);
+	pop.insererFin(NF);
 	
 	cout << pop.ancetreCommun(2, 3) << endl;
 
@@ -145,21 +145,21 @@ void test_enfantCommun() {
 	auto NF = new Noeud<ColorCell>(6, F);
 
 	NA->ancGauche = NB;
-	NA->ancDroit = NC;
+	NA->ancDroite = NC;
 	NB->ancGauche = ND;
-	NB->ancDroit = NE;
+	NB->ancDroite = NE;
 	NC->ancGauche = ND;
-	NC->ancDroit = NF;
+	NC->ancDroite = NF;
 	ND->ancGauche = NE;
-	ND->ancDroit = NF;
+	ND->ancDroite = NF;
 
 	Population<ColorCell> pop;
 	//pop.insererFin(Arbre<ColorCell>(NA));
-	pop.insererFin(Arbre<ColorCell>(NB));
-	pop.insererFin(Arbre<ColorCell>(NC));
-	pop.insererFin(Arbre<ColorCell>(ND));
-	pop.insererFin(Arbre<ColorCell>(NE));
-	pop.insererFin(Arbre<ColorCell>(NF));
+	pop.insererFin(NB);
+	pop.insererFin(NC);
+	pop.insererFin(ND);
+	pop.insererFin(NE);
+	pop.insererFin(NF);
 
 	short int * info =  pop.enfantCommun(4, 5);
 	cout << "enfant = " << *info << endl;
@@ -184,21 +184,21 @@ void test_ancetreETenfant() {
 	auto NF = new Noeud<ColorCell>(6, F);
 
 	NA->ancGauche = NB;
-	NA->ancDroit = NC;
+	NA->ancDroite = NC;
 	NB->ancGauche = ND;
-	NB->ancDroit = NE;
+	NB->ancDroite = NE;
 	NC->ancGauche = ND;
-	NC->ancDroit = NF;
+	NC->ancDroite = NF;
 	ND->ancGauche = NE;
-	ND->ancDroit = NF;
+	ND->ancDroite = NF;
 
 	Population<ColorCell> pop;
-	pop.insererFin(Arbre<ColorCell>(NA));
-	pop.insererFin(Arbre<ColorCell>(NB));
-	pop.insererFin(Arbre<ColorCell>(NC));
-	pop.insererFin(Arbre<ColorCell>(ND));
-	pop.insererFin(Arbre<ColorCell>(NE));
-	pop.insererFin(Arbre<ColorCell>(NF));
+	pop.insererFin(NA);
+	pop.insererFin(NB);
+	pop.insererFin(NC);
+	pop.insererFin(ND);
+	pop.insererFin(NE);
+	pop.insererFin(NF);
 
 	cout << pop.toStringIndividu(1);
 	pop.ancetreETenfantCommuns(1, 2);
@@ -223,16 +223,16 @@ void test_supprimer() {
 
 
 	NA->ancGauche = NB;
-	NA->ancDroit = NC;
+	NA->ancDroite = NC;
 	NB->ancGauche = ND;
-	NB->ancDroit = NE;
+	NB->ancDroite = NE;
 
 	Population<ColorCell> pop;
-	pop.insererFin(Arbre<ColorCell>(NA));
-	pop.insererFin(Arbre<ColorCell>(NB));
-	pop.insererFin(Arbre<ColorCell>(NC));
-	pop.insererFin(Arbre<ColorCell>(ND));
-	pop.insererFin(Arbre<ColorCell>(NE));
+	pop.insererFin(NA);
+	pop.insererFin(NB);
+	pop.insererFin(NC);
+	pop.insererFin(ND);
+	pop.insererFin(NE);
 
 	cout << pop.toStringIndividu(1) << endl;
 
@@ -246,10 +246,58 @@ void test_supprimer() {
 	cout << pop.toStringIndividu(1) << endl;
 }
 
+void test_generer() {
+	cout << "Test generer" << endl;
+	ColorCell D = ColorCell(255, 255, 255);
+	ColorCell F = ColorCell(127, 127, 127);
+	ColorCell E = ColorCell(0, 0, 0);
+
+	ColorCell B = D + E;
+	ColorCell C = D + F;
+	ColorCell A = B + C;
+
+	auto NA = new Noeud<ColorCell>(1, A);
+	auto NB = new Noeud<ColorCell>(2, B);
+	auto NC = new Noeud<ColorCell>(3, C);
+	auto ND = new Noeud<ColorCell>(4, D);
+	auto NE = new Noeud<ColorCell>(5, E);
+	auto NF = new Noeud<ColorCell>(6, F);
+
+	NA->ancGauche = NB;
+	NA->ancDroite = NC;
+	NB->ancGauche = ND;
+	NB->ancDroite = NE;
+	NC->ancGauche = ND;
+	NC->ancDroite = NF;
+	ND->ancGauche = NE;
+	ND->ancDroite = NF;
+
+	Population<ColorCell> pop;
+	pop.insererFin(NA);
+	pop.insererFin(NB);
+	pop.insererFin(NC);
+	pop.insererFin(ND);
+	pop.insererFin(NE);
+	pop.insererFin(NF);
+	for (int i = 0; i < 5; i++) pop.getNextID();
+
+	pop.generer(4);
+	for (pop.fixerTete(); pop.estDansListe(); pop.suivant()) {
+		cout << pop.valeurCourante()->arbre.toString() << endl;
+	}
+}
+
 int main() {
 	setlocale(LC_CTYPE, "fr-FR");
-	test_supprimer();
-
+	/* test_toString();
+	test_Operator_Plus();
+	test_C();
+	test_recherche();
+	test_ancetreCommun();
+	test_enfantCommun();
+	test_ancetreETenfant();
+	test_supprimer(); */
+	test_generer();
 	system("pause");
 	return 0;
 }
--- Population.h
+++ Population.h
@@ -1,43 +1,59 @@
 #pragma once
 #include "NoeudPop.h"
+#include "Noeud.h"
 using namespace std;
 
 template<class T>
 class Population {
 public:
-	Population();														// Constructeur par défaut
-	~Population();														// Destructeur par défaut
+	Population();														// Constructeur par défaut.
+	~Population();														// Destructeur par défaut.
 
-	void insererFin(const Arbre<T>&);									// Insérer un nouveau noeud à la fin
-	void supprimer(const short int _id);								// Suppression d’un noeud
-	NoeudPop<T>* valeurCourante();										// Obtention de la valeur courante
+	void fixerTete();													// Fixer courant à tete.
+	void suivant();														// Changer courant pour le noeud suivant.
+	bool estDansListe();												// Vérifier si courant est dans la liste.
+	short int longueur();												// Obtenir le nombre d’individus.
 
-	void fixerTete();													// Fixer courant à tete
-	void suivant();														// Changer courant pour le noeud suivant
-	bool estDansListe();												// Vérifier si courant est dans la liste
-	bool trouver(const short int _id);									// Trouver un élément précis en fonction de l’id 
-	
-	short int ancetreCommun(const short int, const short int);			// Trouver l’ancêtre commun
-	short int aideAncetreCommun(Arbre<T>& _premier, Arbre<T>& _second);	// Fonction récursive pour trouver un ancetre commun
-	short int * enfantCommun(const short int, const short int);			// Trouver l’enfant commun
-	void ancetreETenfantCommuns(const short int, const short int);		// Afficher les informations pour l’ancêtre et l’enfant en commun
+	bool trouver(const short int);										// Trouver un élément précis en fonction de l’id.
+	NoeudPop<T>* valeurCourante();										// Obtention de la valeur courante.
 
-	string toStringIndividu(const short int _id);						// Obtenir la représentation textuelle d’un arbre
+	void generer(const short int _nb);									// Générer un certain nombre d’individus.
+	void insererFin(T&);												// Insérer un nouvel individu.
+	void supprimer(const short int _id);								// Suppression d’un noeud.
+
+	short int ancetreCommun(const short int, const short int);			// Trouver l’ancêtre commun.
+	short int * enfantCommun(const short int, const short int);			// Trouver l’enfant commun.
+	void ancetreETenfantCommuns(const short int, const short int);		// Afficher les informations pour l’ancêtre et l’enfant en commun.
+
+	string toStringIndividu(const short int _id);						// Obtenir la représentation textuelle d’un arbre.
+
+	// Ces fonctions devraient être private mais pour les test, elles doivent être accessible public//
+	short int getNextID();												// Obtenir le prochain id à utiliser.
+	void insererFin(Noeud<T>*);											// Inserer un noeud.
+
 private:
-	NoeudPop<T>* tete;													// Pointeur tete de liste
-	NoeudPop<T>* courant;												// Pointeur courant de liste
-	NoeudPop<T>* queue;													// Pointeur queue de liste
+	NoeudPop<T>* tete;													// Pointeur tete de liste.
+	NoeudPop<T>* courant;												// Pointeur courant de liste.
+	NoeudPop<T>* queue;													// Pointeur queue de liste.
+	short int nextID;													// Le prochain ID à utiliser.
+	
+	void insererFin(const Arbre<T>&);									// Insérer un nouveau noeud à la fin.
+
+	short int aideAncetreCommun(Arbre<T>& _premier, Arbre<T>& _second);	// Fonction récursive pour trouver un ancetre commun.
+	void aideGenerer();													// Fonction générant un nouvel individu.
+
 };
 
 // CONSTRUCTEURS DESTRUCTEURS /////////////////////////////////////////////////////////////////////
 
-// Constructeur par défaut
+// Constructeur par défaut.
 template<class T>
 Population<T>::Population() {
 	tete = courant = queue = new NoeudPop<T>();
+	nextID = 1;
 }
 
-// Destructeur par défaut
+// Destructeur par défaut.
 template<class T>
 Population<T>::~Population(){
 	while (tete != nullptr)
@@ -48,11 +64,39 @@ Population<T>::~Population(){
 	}
 }
 
-// FONCTIONS //////////////////////////////////////////////////////////////////////////////////////
+// GETTERS SETTERS ////////////////////////////////////////////////////////////////////////////////
 
 /*
-* Insérer un Arbre à la fin de la liste
-* @param _arbre : un Arbre de cellules
+* Getter du prochain ID à utiliser.
+* La variable nextID est incrémenté de 1 à chaque appel de la fonction.
+* @returns le prochain ID à utiliser.
+*/
+template<class T>
+short int Population<T>::getNextID() {
+	nextID++;
+	return nextID;
+}
+
+/*
+* Getter de la longueur de la Population. Soit le nombre d’individus qui la compose.
+* @returns Le nombre total d’individus.
+*/
+template<class T>
+short int Population<T>::longueur() {
+	short int compteur = 0;
+	for (fixerTete(); estDansListe(); suivant()) {
+		compteur++;
+	}
+	return compteur;
+}
+
+// FONCTIONS //////////////////////////////////////////////////////////////////////////////////////
+
+// INSERER SUPPRIMER
+
+/*
+* Insérer un Arbre à la fin de la liste.
+* @param _arbre : un Arbre de cellules.
 */
 template<class T>
 void Population<T>::insererFin(const Arbre<T>& _arbre) {
@@ -60,23 +104,43 @@ void Population<T>::insererFin(const Arbre<T>& _arbre) {
 }
 
 /*
-* Suppression d’un arbre de la liste chaînée
-* @param _id : l’id recherché
+* Insérer une cellule en tant qu’individu.
+* @param _cell : la cellule à insérer dans la population.
+*/
+template<class T>
+void Population<T>::insererFin(T& _cell) {
+	Noeud<T>* newNoeud = new Noeud<ColorCell>(getNextID(), _cell);
+	this->insererFin(Arbre<T>(newNoeud));
+}
+
+/* 
+* Insérer un noeud en tant qu’individu.
+* @param _noeud : le noeud à insérer dans la population.
+*/
+template<class T>
+void Population<T>::insererFin(Noeud<T>* _noeud) {
+	insererFin(Arbre<T>(_noeud));
+}
+
+/*
+* Suppression d’un arbre de la liste chaînée.
+* @param _id : l’id recherché.
 */
 template<class T>
 void Population<T>::supprimer(const short int _id) {
 	// Il faut supprimer cet id parmi les ancêtres gauches et droits de tous les arbres
 	for (fixerTete(); estDansListe(); suivant()) {
 		// On récupère la valeur courante
-		NoeudPop<T>* noeudCourant = valeurCourante();
-		Arbre<T> arbreCourant = valeurCourante()->arbre;
-		// On vérifie qu’il y a un ancêtre à gauche et puis on vérifie si c’est l’id que l’on recherche
-		if (arbreCourant.racine->ancGauche != nullptr && arbreCourant.racine->ancGauche->getID() == _id) {
-			arbreCourant.racine->ancGauche = nullptr;
+		NoeudPop<T>* nCourant = valeurCourante();
+		// On vérifie qu’il y a un ancêtre à cellG et puis on vérifie si c’est l’id que l’on recherche
+		if (nCourant->getR_Anc_Gauche() != nullptr && nCourant->getR_Anc_Gauche()->getID() == _id) {
+			// getR() et non getR_Anc_Gauche afin de pouvoir modifier la valeur
+			nCourant->getR()->ancGauche = nullptr;
 		}
-		// On vérifie qu’il y a un ancêtre à droite et puis on vérifie si c’est l’id que l’on recherche
-		if (arbreCourant.racine->ancDroit != nullptr && noeudCourant->arbre.racine->ancDroit->getID() == _id) {
-			noeudCourant->arbre.racine->ancDroit = nullptr;
+		// On vérifie qu’il y a un ancêtre à cellD et puis on vérifie si c’est l’id que l’on recherche
+		if (nCourant->getR_Anc_Droit() != nullptr && nCourant->getR_Anc_Droit()->getID() == _id) {
+			// getR() et non getR_Anc_Droit afin de pouvoir modifier la valeur
+			nCourant->getR()->ancDroite = nullptr;
 		}
 	}
 
@@ -94,37 +158,43 @@ void Population<T>::supprimer(const short int _id) {
 	}
 }
 
+// OPÉRATIONS BASIQUES
+
 /*
-* Obtenir la valeur courante du tableau
+* Obtenir la valeur courante du tableau.
+* @returns le pointeur suivant.
 */
 template<class T>
 NoeudPop<T>* Population<T>::valeurCourante() {
 	return courant->suivant;
 }
 
-// Transporte le pointeur courant à la tête de la liste
+// Transporte le pointeur courant à la tête de la liste.
 template<class T>
 void Population<T>::fixerTete() {
 	courant = tete;
 }
 
-// Transporte le pointeur courant au noeud suivant dans la liste
+// Transporte le pointeur courant au noeud suivant dans la liste.
 template<class T>
 void Population<T>::suivant() {
 	if(courant != nullptr) courant = courant->suivant;
 }
 
-// Retourne true si le pointeur courant est la liste
+/*
+* Vérifie que le pointeur courant est la liste.
+* @returns true si le pointeur courant est dans la liste.
+*/
 template<class T>
 bool Population<T>::estDansListe() {
 	return (courant != nullptr) && (courant->suivant != nullptr);
 }
 
 /*
-* Trouver l’id recherché parmi les Arbres de la population
-* @param _id : l’id recherché
-* @returns true dès que le pointeur courant est sur l’arbre dont la racine a l’ID = _id.
-* false dès que la liste est parcourue sans plus de résultat.
+* Trouver l’id recherché parmi les Arbres de la population.
+* @param _id :	l’id recherché.
+* @returns		true dès que le pointeur courant est sur l’arbre dont la racine a l’ID = _id.
+*				false dès que la liste est parcourue sans plus de résultat.
 */
 template<class T>
 bool Population<T>::trouver(const short int _id) {
@@ -133,7 +203,7 @@ bool Population<T>::trouver(const short int _id) {
 		// On récupère la valeur courante
 		NoeudPop<T>* noeudCourant = valeurCourante();
 		// On vérifie l’id courant
-		if (noeudCourant->arbre.racine->getID() == _id) {
+		if (noeudCourant->arbre.getR_ID() == _id) {
 			return true;
 		}
 	}
@@ -141,11 +211,13 @@ bool Population<T>::trouver(const short int _id) {
 	return false;
 }
 
+// FONCTIONS ARBOTIC
+
 /*
-* Fonction permettant de trouver l’ancêtre commun de deux individus
-* @param _premierID : l’id du premier individu
-* @param _secondID : l’id du second individu
-* @returns l’id de l’ancêtre en commun
+* Fonction permettant de trouver l’ancêtre commun de deux individus.
+* @param _premierID :	l’id du premier individu.
+* @param _secondID :	l’id du second individu.
+* @returns				l’id de l’ancêtre en commun.
 */
 template<class T>
 short int Population<T>::ancetreCommun(const short int _premierID, const short int _secondID) {
@@ -175,11 +247,11 @@ short int Population<T>::ancetreCommun(const short int _premierID, const short i
 }
 
 /*
-* Fonction récursive pour trouver un ancêtre commun à partir de deux arbres donnés
-* @param _premierID : l’arbre qui sera inspecter noeud par noeud.
-* @param _secondID : l’arbre qui sera parcouru à chaque recherche.
-* @returns un nombre négatif dans le cas où il n’y a pas d’ancêtre commun
-* sinon l’id de l’ancêtre commun
+* Fonction récursive pour trouver un ancêtre commun à partir de deux arbres donnés.
+* @param _premierID :	l’arbre qui sera inspecter noeud par noeud.
+* @param _secondID :	l’arbre qui sera parcouru à chaque recherche.
+* @returns				un nombre négatif dans le cas où il n’y a pas d’ancêtre commun.
+*						sinon l’id de l’ancêtre commun.
 */
 template<class T>
 short int Population<T>::aideAncetreCommun(Arbre<T>& _premier, Arbre<T>& _second) {
@@ -187,45 +259,46 @@ short int Population<T>::aideAncetreCommun(Arbre<T>& _premier, Arbre<T>& _second
 	if (_premier.estVide()) return -1;
 
 	// Chercher l’id dans le second arbre
-	if (_second.recherche(_premier.racine->getID()) <= 0) {
+	if (_second.recherche(_premier.getR_ID()) <= 0) {
 		// Si non trouvé
 
 		short int estGauche = -1, estDroit =-1;
-		// On cherche avec l’ancêtre gauche
-		if (_premier.racine->ancGauche != nullptr) {
-			// On récupère l’ancêtre de gauche
-			if (trouver(_premier.racine->ancGauche->getID())) {
+		// On cherche avec l’ancêtre cellG
+		if (_premier.getR_Anc_Gauche() != nullptr) {
+			// On récupère l’ancêtre de cellG
+			if (trouver(_premier.getR_Anc_Gauche()->getID())) {
 				Arbre<T>& gauche = valeurCourante()->arbre;
-				// On cherche à gauche
+				// On cherche à cellG
 				estGauche = aideAncetreCommun(gauche, _second);
 			}
 		}
 
-		// Puis si on ne l’a pas trouvé à gauche, on cherche avec l’ancêtre droit.
-		if (_premier.racine->ancDroit != nullptr && estGauche < 0) {
+		// Puis si on ne l’a pas trouvé à cellG, on cherche avec l’ancêtre droit.
+		if (_premier.getR_Anc_Droit() != nullptr && estGauche < 0) {
 			// On récupère l’ancêtre de droit
-			if (trouver(_premier.racine->ancDroit->getID())) {
+			if (trouver(_premier.getR_Anc_Droit()->getID())) {
 				Arbre<T>& droit = valeurCourante()->arbre;
-				// On cherche à droite
+				// On cherche à cellD
 				estDroit = aideAncetreCommun(droit, _second);
-				// Peu importe le résultat, après avoir cherché à droite, on arrête la recherche
+				// Peu importe le résultat, après avoir cherché à cellD, on arrête la recherche
 				return estDroit;
 			}
 		}
-		// Si on a trouvé à gauche
+		// Si on a trouvé à cellG
 		return estGauche; 
 
 	// On a trouvé un résultat 
 	} else {
-		return _premier.racine->getID();
+		return _premier.getR_ID();
 	}
 }
 
 /*
-* Fonction permettant de trouver l’enfant commun de deux individus
-* @param _premierID : l’id du premier individu
-* @param _secondID : l’id du second individu
-* @returns l’id de l’enfant en commun et la différence de génération entre les deux id _premierID et _secondID
+* Fonction permettant de trouver l’enfant commun de deux individus.
+* @param _premierID :	l’id du premier individu.
+* @param _secondID :	l’id du second individu.
+* @returns				l’id de l’enfant en commun et la différence de génération 
+						entre les deux id _premierID et _secondID.
 */
 template<class T>
 short int * Population<T>::enfantCommun(const short int _premierID, const short int _secondID) {
@@ -245,7 +318,7 @@ short int * Population<T>::enfantCommun(const short int _premierID, const short 
 			// Si le second élément est trouvé
 			if (genSecond >= 0) {
 				// On retient l’id de l’arbre
-				infoEnfant[0] = tmp.racine->getID();
+				infoEnfant[0] = tmp.getR_ID();
 
 				// On calcule la différence de génération
 				if (genPremier > genSecond) {
@@ -265,9 +338,9 @@ short int * Population<T>::enfantCommun(const short int _premierID, const short 
 }
 
 /*
-* Afficher les informations sur l’ancêtre et l’enfant en commun pour deux individus
-* @param _premierID : le premier individu à utiliser pour les recherches
-* @param _secondID : le second individu à utiliser pour les recherches
+* Afficher les informations sur l’ancêtre et l’enfant en commun pour deux individus.
+* @param _premierID :	le premier individu à utiliser pour les recherches.
+* @param _secondID :	le second individu à utiliser pour les recherches.
 */
 template<class T>
 void Population<T>::ancetreETenfantCommuns(const short int _premierID, const short int _secondID) {
@@ -297,9 +370,67 @@ void Population<T>::ancetreETenfantCommuns(const short int _premierID, const sho
 }
 
 /*
-* Fonction renvoyant la représentation textuelle d’un individu faisant partie de cette population
-* @param _id : l’id d’un individu
-* @returns le string de l’arborescence d’un individu ou un message d’erreur.
+* Fonction appellant un nombre de fois paramétré la génération d’un nouvel individu.
+* @param _nb : le nombre de nouveaux individus à générer.
+*/
+template<class T>
+void Population<T>::generer(const short int _nb) {
+	for (short int i = 0; i < _nb; i++) {
+		aideGenerer();
+	}
+}
+
+// Fonction permettant de générer un nouvel individu à partir de deux parents déterminés aléatoirement.
+template<class T>
+void Population<T>::aideGenerer() {
+	// Avoir deux numéros différents pour les parents de la cellule
+	short int randG = rand() % longueur();
+	short int randD = randG;
+	do {
+		randD = rand() % longueur();
+	} while (randD == randG);
+
+
+	// Obtenir les NoeudPop correspondant aux emplacements trouvés
+	NoeudPop<T>* NPopG = nullptr;
+	NoeudPop<T>* NPopD = nullptr;
+	for (fixerTete(); estDansListe(); suivant()) {
+
+		// Dès que randG est complètement décrémenté, on récupère la valeur courante
+		if (randG == 0) {
+			NPopG = valeurCourante();
+		}
+
+		// Dès que randD est complètement décrémenté, on récupère la valeur courante
+
+		if (randD == 0) {
+			NPopD = valeurCourante();
+		}
+
+		if (randG < 0 && randD < 0) {
+			break;
+		}
+		// On décrémente les compteurs
+		randG--;
+		randD--;
+	}
+
+	// On récupère les valeurs
+	T cellG = NPopG->getR_cell();
+	T cellD = NPopD->getR_cell();
+
+	// On créé une nouvelle cellule en fonction des valeurs des cellules parents, gauche et droite
+	T newIndividu = cellG + cellD;
+	Noeud<T>* newNoeud = new Noeud<T>(getNextID(), newIndividu);
+	newNoeud->ancGauche = NPopG->getR();
+	newNoeud->ancDroite = NPopD->getR();
+	insererFin(newNoeud);
+}
+
+/*
+* Fonction renvoyant la représentation textuelle d’un individu faisant partie de cette population.
+* @param _id :	l’id d’un individu.
+* @returns		le string de l’arborescence d’un individu ou un message d’erreur.
 */
 template<class T>
 string Population<T>::toStringIndividu(const short int _id) {
